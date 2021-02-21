e7a337f1333704eae48926f6cd7d77496b5d7f45
Ryan Hartlage <ryanplusplus@gmail.com>
2021-02-21 12:26:59 -0500

    Added defaults.mk

---
+++ defaults.mk
@@ -0,0 +1,37 @@
+ASFLAGS := \
+  -mcpu=$(CPU) \
+  -march=$(ARCH) \
+  -mthumb \
+  -g2 \
+
+CPPFLAGS := \
+  --specs=nano.specs \
+  -mcpu=$(CPU) \
+  -march=$(ARCH) \
+  -mthumb \
+  -g \
+  -g2 \
+  -Os \
+  -fdata-sections \
+  -ffunction-sections \
+  -Wall \
+  -Wextra \
+  -Werror \
+  -Wfatal-errors \
+  $(addprefix -D,$(DEFINES)) \
+
+CFLAGS := \
+  -std=c99 \
+
+CXXFLAGS := \
+  -fno-rtti \
+  -fno-exceptions \
+  -fno-non-call-exceptions \
+  -fno-use-cxa-atexit \
+  -Weffc++ \
+  -std=c++17 \
+
+LDFLAGS := \
+  -Og \
+  --gc-sections \
+  -Map=$(BUILD_DIR)/$(TARGET).map \
