76fc37876cbd1b150e123bf42af81795e3505fbf
Ryan Hartlage <ryanplusplus@gmail.com>
2021-02-21 13:55:26 -0500

    Added support for 'other' debug adapters

--- debug-adapter.mk
+++ debug-adapter.mk
@@ -2,8 +2,10 @@ ifeq ($(DEBUG_ADAPTER),openocd)
 include $(dir $(realpath $(lastword $(MAKEFILE_LIST))))openocd.mk
 else ifeq ($(DEBUG_ADAPTER),black_magic)
 include $(dir $(realpath $(lastword $(MAKEFILE_LIST))))black-magic.mk
+else ifeq ($(DEBUG_ADAPTER),other)
+include $(dir $(realpath $(lastword $(MAKEFILE_LIST))))other.mk
 else
-$(error "DEBUG_ADAPTER must be openocd or black_magic")
+$(error "DEBUG_ADAPTER must be openocd, black_magic, or other")
 endif
 
 $(BUILD_DIR)/$(TARGET).svd: $(SVD)
