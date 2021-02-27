e0a978e1ac3e0a85657db0f78ad7801e282e27b4
Ryan Hartlage <ryanplusplus@gmail.com>
2021-02-27 12:18:19 -0500

    Fixed includes

--- tools.mk
+++ tools.mk
@@ -1,2 +1,4 @@
-include $(dir $(realpath $(lastword $(MAKEFILE_LIST))))gcc/tools.mk
-include $(dir $(realpath $(lastword $(MAKEFILE_LIST))))debug-adapter.mk
+__arm_none_eabi_gcc_tools_path := $(dir $(realpath $(lastword $(MAKEFILE_LIST))))
+
+include $(__arm_none_eabi_gcc_tools_path)gcc/tools.mk
+include $(__arm_none_eabi_gcc_tools_path)debug-adapter.mk
