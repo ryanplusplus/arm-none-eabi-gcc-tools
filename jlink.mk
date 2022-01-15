1dade80379e5bec7e04206cabb406e6e942e6ad0
Ryan Hartlage <ryanplusplus@gmail.com>
2022-01-15 08:36:39 -0500

    Suppress license pop-up for openSDA jlink firmware

--- jlink.mk
+++ jlink.mk
@@ -2,19 +2,19 @@ ifeq ($(JLINK_DEVICE),)
 $(error "JLINK_DEVICE must be defined")
 endif
 
-.PHONY: suppress-jlink-edu-popup
-suppress-jlink-edu-popup:
-	@type lua > /dev/null 2>&1 && lua $(__arm_none_eabi_gcc_tools_path)/script/suppress-jlink-edu-popup.lua > /dev/null 2>&1 || true
+.PHONY: suppress-jlink-license-popup
+suppress-jlink-license-popup:
+	@type lua > /dev/null 2>&1 && lua $(__arm_none_eabi_gcc_tools_path)/script/suppress-jlink-license-popup.lua > /dev/null 2>&1 || true
 
 .PHONY: debug-deps
-debug-deps: $(BUILD_DIR)/$(TARGET).elf $(BUILD_DIR)/$(TARGET).svd suppress-jlink-edu-popup
+debug-deps: $(BUILD_DIR)/$(TARGET).elf $(BUILD_DIR)/$(TARGET).svd suppress-jlink-license-popup
 
 .PHONY: upload
-upload: $(BUILD_DIR)/upload.jlink suppress-jlink-edu-popup
+upload: $(BUILD_DIR)/upload.jlink suppress-jlink-license-popup
 	@JLinkExe -NoGui 1 -device $(JLINK_DEVICE) -if SWD -autoconnect 1 -speed 4000 -CommandFile $<
 
 .PHONY: erase
-erase: $(BUILD_DIR)/erase.jlink suppress-jlink-edu-popup
+erase: $(BUILD_DIR)/erase.jlink suppress-jlink-license-popup
 	@JLinkExe -NoGui 1 -device $(JLINK_DEVICE) -if SWD -autoconnect 1 -speed 4000 -CommandFile $<
 
 .PHONY: $(BUILD_DIR)/upload.jlink
--- script/suppress-jlink-license-popup.lua
+++ script/suppress-jlink-license-popup.lua
@@ -1,7 +1,10 @@
-local date = os.date('*t')
+local keys = {
+  'LicenseEDUMini_DontShowAgainToday',
+  'LicenseOpenSDA_DontShowAgainToday'
+}
 
+local date = os.date('*t')
 local filename = os.getenv('HOME') .. '/.config/SEGGER/SEGGER_REG_HKEY_CURRENT_USER.xml'
-local key = 'LicenseEDUMini_DontShowAgainToday'
 local value = ('0x07E%01X%02X%02X'):format(date.year - 2016, date.month, date.day)
 
 local contents do
@@ -10,7 +13,9 @@ local contents do
   read_file:close()
 end
 
-contents = contents:gsub(key .. '="0x07E....."', key .. '="' .. value .. '"')
+for _, key in ipairs(keys) do
+  contents = contents:gsub(key .. '="0x07E....."', key .. '="' .. value .. '"')
+end
 
 local write_file = io.open(filename, 'w+')
 write_file:write(contents)
