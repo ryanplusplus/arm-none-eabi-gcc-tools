684e015cce8c4fdf8c26fa5369aa21945e2ca2f1
Ryan Hartlage <ryanplusplus@gmail.com>
2022-03-06 09:48:53 -0500

    Added generate_jlink_upload_rule

--- jlink.mk
+++ jlink.mk
@@ -2,6 +2,23 @@ ifeq ($(JLINK_DEVICE),)
 $(error "JLINK_DEVICE must be defined")
 endif
 
+# $1 Rule name
+# $2 File
+define generate_jlink_upload_rule
+.PHONY: $$(BUILD_DIR)/$1.jlink
+$$(BUILD_DIR)/$1.jlink: $2
+	@mkdir -p $(dir $$@)
+	@echo r > $$@
+	@echo h >> $$@
+	@echo loadfile $$< >> $$@
+	@echo r >> $$@
+	@echo exit >> $$@
+
+.PHONY: upload
+$1: $(BUILD_DIR)/$1.jlink suppress-jlink-license-popup
+	@JLinkExe -NoGui 1 -device $(JLINK_DEVICE) -if SWD -autoconnect 1 -speed 4000 -CommandFile $$<
+endef
+
 .PHONY: suppress-jlink-license-popup
 suppress-jlink-license-popup:
 	@type lua > /dev/null 2>&1 && lua $(__arm_none_eabi_gcc_tools_path)/script/suppress-jlink-license-popup.lua > /dev/null 2>&1 || true
@@ -9,22 +26,7 @@ suppress-jlink-license-popup:
 .PHONY: debug-deps
 debug-deps: $(BUILD_DIR)/$(TARGET).elf $(BUILD_DIR)/$(TARGET).svd suppress-jlink-license-popup
 
-.PHONY: upload
-upload: $(BUILD_DIR)/upload.jlink suppress-jlink-license-popup
-	@JLinkExe -NoGui 1 -device $(JLINK_DEVICE) -if SWD -autoconnect 1 -speed 4000 -CommandFile $<
-
-.PHONY: erase
-erase: $(BUILD_DIR)/erase.jlink suppress-jlink-license-popup
-	@JLinkExe -NoGui 1 -device $(JLINK_DEVICE) -if SWD -autoconnect 1 -speed 4000 -CommandFile $<
-
-.PHONY: $(BUILD_DIR)/upload.jlink
-$(BUILD_DIR)/upload.jlink: $(BUILD_DIR)/$(TARGET).hex
-	@mkdir -p $(dir $@)
-	@echo r > $@
-	@echo h >> $@
-	@echo loadfile $< >> $@
-	@echo r >> $@
-	@echo exit >> $@
+$(eval $(call generate_jlink_upload_rule,upload,$(BUILD_DIR)/$(TARGET).hex))
 
 .PHONY: $(BUILD_DIR)/erase.jlink
 $(BUILD_DIR)/erase.jlink:
@@ -33,3 +35,7 @@ $(BUILD_DIR)/erase.jlink:
 	@echo h >> $@
 	@echo erase >> $@
 	@echo exit >> $@
+
+.PHONY: erase
+erase: $(BUILD_DIR)/erase.jlink suppress-jlink-license-popup
+	@JLinkExe -NoGui 1 -device $(JLINK_DEVICE) -if SWD -autoconnect 1 -speed 4000 -CommandFile $<
