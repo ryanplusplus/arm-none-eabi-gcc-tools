2e6799f16ec1e31971a0ed29e9c96d16f9647f17
Ryan Hartlage <ryanplusplus@gmail.com>
2022-05-09 20:35:20 -0400

    Automagically suppress jlink edu popup

--- jlink.mk
+++ jlink.mk
@@ -2,23 +2,27 @@ ifeq ($(JLINK_DEVICE),)
 $(error "JLINK_DEVICE must be defined")
 endif
 
+.PHONY: suppress-jlink-edu-popup
+suppress-jlink-edu-popup:
+	@type lua && lua $(__arm_none_eabi_gcc_tools_path)/script/suppress-jlink-edu-popup.lua
+
 .PHONY: debug-deps
 debug-deps: $(BUILD_DIR)/$(TARGET).elf $(BUILD_DIR)/$(TARGET).svd
 
 .PHONY: upload
-upload: $(BUILD_DIR)/upload.jlink $(BUILD_DIR)/$(TARGET).hex
+upload: $(BUILD_DIR)/upload.jlink suppress-jlink-edu-popup
 	@JLinkExe -device $(JLINK_DEVICE) -if SWD -autoconnect 1 -speed 4000 -CommandFile $<
 
 .PHONY: erase
-erase: $(BUILD_DIR)/erase.jlink
+erase: $(BUILD_DIR)/erase.jlink suppress-jlink-edu-popup
 	@JLinkExe -device $(JLINK_DEVICE) -if SWD -autoconnect 1 -speed 4000 -CommandFile $<
 
 .PHONY: $(BUILD_DIR)/upload.jlink
-$(BUILD_DIR)/upload.jlink:
+$(BUILD_DIR)/upload.jlink: $(BUILD_DIR)/$(TARGET).hex
 	@mkdir -p $(dir $@)
 	@echo r > $@
 	@echo h >> $@
-	@echo loadfile $(BUILD_DIR)/$(TARGET).hex >> $@
+	@echo loadfile $< >> $@
 	@echo r >> $@
 	@echo exit >> $@
 
